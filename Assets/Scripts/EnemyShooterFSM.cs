55ab267a9d4a8d3de4e3420a8a80ca327efb11e6
Musiichuk Dmitrij <msijcukdmitrij@gmail.com>
2026-05-09 21:50:56 +0300

    Fix: resolved background problem and cleaned up scene hierarchy.

---
+++ Assets/Scripts/EnemyShooterFSM.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyShooterFSM : MonoBehaviour
+{
+    public enum EnemyState { MovingToPosition, Shooting, Leaving }
+
+    [Header("Налаштування стану: Moving")]
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float targetStopY = 3f; 
+
+    [Header("Налаштування стану: Shooting")]
+    [SerializeField] private float fireRate = 2f; 
+    [SerializeField] private float timeToShoot = 3f;
+    [SerializeField] private GameObject bulletPrefab; 
+    [SerializeField] private Transform firePoint; 
+
+    [Header("Налаштування стану: Leaving")]
+    [SerializeField] private float leaveSpeed = 5f;
+
+    private EnemyState currentState = EnemyState.MovingToPosition;
+    private bool isShootingCoroutineRunning = false;
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.MovingToPosition:
+                MoveDown();
+                break;
+            case EnemyState.Shooting:
+                if (!isShootingCoroutineRunning)
+                {
+                    StartCoroutine(ShootingRoutine());
+                }
+                break;
+            case EnemyState.Leaving:
+                LeaveScreen();
+                break;
+        }
+    }
+
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+
+        if (transform.position.y <= targetStopY)
+        {
+            currentState = EnemyState.Shooting;
+        }
+    }
+
+    private IEnumerator ShootingRoutine()
+    {
+        isShootingCoroutineRunning = true;
+        float currentShootTime = 0f;
+        
+        while (currentState == EnemyState.Shooting && currentShootTime < timeToShoot)
+        {
+            if (bulletPrefab && firePoint)
+            {
+                Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+            }
+            
+            yield return new WaitForSeconds(1f / fireRate);
+            currentShootTime += (1f / fireRate);
+        }
+        
+        currentState = EnemyState.Leaving;
+        isShootingCoroutineRunning = false;
+    }
+
+    private void LeaveScreen()
+    {
+        transform.Translate(new Vector3(1, 1, 0) * leaveSpeed * Time.deltaTime);
+    }
+}
